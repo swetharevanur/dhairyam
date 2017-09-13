ac3eaf058dec9738f9094c9de1725c2dcb0c2424
swetharevanur <swetharevanur@Swethas-MacBook-Pro-2.local>
2017-09-12 18:26:46 -0700

    added form and POST request to handle input

--- test/app.js
+++ test/app.js
@@ -28,13 +28,50 @@ app.use(bodyParser.urlencoded({extended: false}))
 // set static path for resources 
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+const users = [
+	{
+		id: 1,
+		first_name: "Bill",
+		last_name: "Hewlett",
+		email: "bill@gmail.com"
+	},
+	{
+		id: 2,
+		first_name: "Dave",
+		last_name: "Packard",
+		email: "dave@gmail.com"
+	},
+	{
+		id: 3,
+		first_name: "John",
+		last_name: "Doe",
+		email: "john@gmail.com"
+	}
+]
+
 // GET request
 app.get('/', (req, res) => {
 	res.render('index', {
-		title: 'Customers'
+		title: 'Customers',
+		users: users
 	});
+})
+
+// POST request
+app.post('/users/add', (req, res) => {
+	const newUser = { // create new user from form input
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		email: req.body.email
+	} 
+
+	console.log(newUser);
 })
 
 app.listen(port, hostname, () => {
 	console.log('Server started on port ' + port);
 })
+
+
+
