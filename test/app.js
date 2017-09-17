01fa87ad15cb0820475af8449df2988597d9f478
swetharevanur <swetharevanur@Swethas-MacBook-Pro-2.local>
2017-09-17 12:39:07 -0700

    added in simple error checking with express-validator; error messages are rendered on website with GET request

--- test/app.js
+++ test/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path'); // core module so no need for npm install
+const expressValidator = require('express-validator');
 
 const hostname = 'localhost';
 const port = '3000';
@@ -28,6 +29,24 @@ app.use(bodyParser.urlencoded({extended: false}))
 // set static path for resources 
 app.use(express.static(path.join(__dirname, 'public')))
 
+// load express-validator's error formatting middleware
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value, location) {
+		const namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+
+		while (namespace.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+}));
 
 const users = [
 	{
@@ -56,22 +75,38 @@ app.get('/', (req, res) => {
 		title: 'Customers',
 		users: users
 	});
-})
+});
+
+
 
 // POST request
 app.post('/users/add', (req, res) => {
-	const newUser = { // create new user from form input
-		first_name: req.body.first_name,
-		last_name: req.body.last_name,
-		email: req.body.email
-	} 
+	// specify validator rules
+	req.checkBody('first_name', 'First name is required').notEmpty();
+	req.checkBody('last_name', 'Last name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
 
-	console.log(newUser);
-})
+	const errors = req.validationErrors();
+	if (errors) {
+		res.render('index', {
+			title: 'Customers',
+			users: users,
+			errors: errors
+		});
+	} else {
+		const newUser = { // create new user object from form input
+			first_name: req.body.first_name, // comes from first_name field
+			last_name: req.body.last_name,
+			email: req.body.email
+		}
+
+		console.log('SUCCESS');
+	} 	
+});
 
 app.listen(port, hostname, () => {
 	console.log('Server started on port ' + port);
-})
+});
 
 
 
